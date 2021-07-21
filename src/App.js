8fde86264499f2ea46662ed3195f15fa89d61b7e
Casey Trombley <casey@algaecal.com>
2021-07-20 21:09:13 -0400

    added files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import logo from './logo.svg';
 import Starships from './components/starships';
+import audio from './starwars.mp3';
 import './App.css';
 
 function App() {
-  return (
-    <div className="app">
-      <header className="header">
-          <img
-              src={logo}
-              className="logo"
-              alt="logo"
-              onClick={refreshPage}
-          />
-      </header>
-        <div className="main">
-            <Starships />
+    return (
+        <div className="app">
+            <header className="header">
+                <img
+                  src={logo}
+                  className="logo"
+                  alt="logo"
+                  onClick={starWars}
+                />
+            </header>
+            <div className="main">
+                <Starships />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
-function refreshPage() {
-    window.location.reload(false);
+function starWars() {
+    // window.location.reload(false);
+    new Audio(audio).play();
 }
+
 
 export default App;
